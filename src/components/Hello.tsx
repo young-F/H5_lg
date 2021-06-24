c8f60ae7ab26298be76f5350b539b357396d56d6
young-F <540848404@qq.com>
2021-06-24 13:09:54 +0800

    project init

---
+++ src/components/Hello.tsx
@@ -0,0 +1,10 @@
+interface HelloProps {
+    msg: string;
+}
+const Hello = (props: HelloProps) => {
+    return (
+        <h1>{props.msg}</h1>
+    )
+}
+Hello.props = ['msg']
+export default Hello
